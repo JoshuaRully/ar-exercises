8659eee544a4f935583277e46e4dd82a650f2059
Joshua Rully <joshrully@gmail.com>
2021-03-29 22:20:07 +0000

    modify Store model and add employee data

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -8,4 +8,33 @@ require_relative './exercise_5'
 puts "Exercise 6"
 puts "----------"
 
-# Your code goes here ...
+@store1.employees.create(
+  first_name: "Khurram", 
+  last_name: "Virani",
+  hourly_rate: 60
+)
+@store1.employees.create(
+  first_name: "Jimbo",
+  last_name: "Jenkins",
+  hourly_rate: 25
+)
+@store1.employees.create(
+  first_name: "Juan",
+  last_name: "Sanchez",
+  hourly_rate: 100
+)
+@store2.employees.create(
+  first_name: "Leroy",
+  last_name: "Sturbys",
+  hourly_rate: 40
+)
+@store2.employees.create(
+  first_name: "Matilda",
+  last_name: "Midtbo",
+  hourly_rate: 75
+)
+@store2.employees.create(
+  first_name: "Terrence",
+  last_name: "Crews",
+  hourly_rate: 150
+)
