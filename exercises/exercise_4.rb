4b03cfdde00ebc9c99f0d3d5b4ff7c801306645b
Joshua Rully <joshrully@gmail.com>
2021-03-29 21:40:56 +0000

    query for stores based on clothing and revenue

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -6,4 +6,22 @@ require_relative './exercise_3'
 puts "Exercise 4"
 puts "----------"
 
-# Your code goes here ...
+user = Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+user = Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+user = Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+
+@mens_stores = Store.where(mens_apparel: true)
+puts "~~~~Mens Stores~~~~"
+@mens_stores.map {
+  |store| 
+  puts "#{store.name}: #{store.annual_revenue}" 
+}
+
+puts "~~~Womens Stores~~~"
+@womens_stores = Store.where(womens_apparel: true)
+@womens_stores.map {
+  |store| 
+  if store.annual_revenue < 1000000 
+    puts "#{store.name}: #{store.annual_revenue}" 
+  end
+}
