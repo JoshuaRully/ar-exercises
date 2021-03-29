36820c1d3ae653b881382990f81e2a1fa998fb45
Joshua Rully <joshrully@gmail.com>
2021-03-29 18:38:37 +0000

    add 3 stores and check that 3 stores in db

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -3,4 +3,16 @@ require_relative '../setup'
 puts "Exercise 1"
 puts "----------"
 
-# Your code goes below here ...
+user = Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
+user = Store.create(name: "Richmond", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
+user = Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
+
+puts Store.count  
+
+# create_table :stores do |t|
+#   t.column :name, :string
+#   t.column :annual_revenue, :integer
+#   t.column :mens_apparel, :boolean
+#   t.column :womens_apparel, :boolean
+#   t.timestamps null: false
+# end
